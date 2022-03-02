cae4a13cacdd2da7519343df43956c2628a51136
Michael Kane <kaneplusplus@gmail.com>
2022-03-02 08:35:44 -0500

    Test change.

--- tests/testthat/test-stationary-distribution.r
+++ tests/testthat/test-stationary-distribution.r
@@ -1,6 +1,7 @@
 context("Stationary distribution.")
 
-# From http://www.stat.yale.edu/~pollard/Courses/251.spring2013/Handouts/Chang-MarkovChains.pdf
+# From 
+# http://www.stat.yale.edu/~pollard/Courses/251.spring2013/Handouts/Chang-MarkovChains.pdf.
 P <- matrix(c(0, 1, 0, 1/3, 0, 2/3, 1/3, 1/3, 1/3), nrow = 3, byrow = TRUE)
 
 expect_equal(stationary_distr(P), 
